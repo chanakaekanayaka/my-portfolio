47b86049e3c5c34260c05b168529510a65978189
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2026-01-22 23:44:25 +0530

    Change styles of navbar✏️

--- app/components/Navbae.jsx
+++ app/components/Navbae.jsx
@@ -5,10 +5,7 @@ import Image from 'next/image'
 const Navbae = () => {
   return (
     <>
-    <div className='fixed top-0 right-0 left-0 w-full -z-10 translate-y-[-80]'>
-    <Image src={assets.header_bg_color} alt='' className='w-full'/>
-
-    </div>
+    
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
     flex items-center justify-between z-50'>
         <a href='#top' className='animate-bounce'>
@@ -20,7 +17,7 @@ const Navbae = () => {
 
         </a>
         <ul className='hidden md:flex items-center gap-6 lg:gap-8 
-        rounded-full px-12 py-4  font-medium text-teal-700 shadow-lg bg-opacity-50 hover:bg-teal-50'>
+        rounded-full px-12 py-4  font-medium text-teal-700 shadow-lg bg-opacity-50 '>
             <li><a href='#top' className='  transition-colors hover:text-teal-300 '>Home</a></li>
             <li><a href='#about' className='transition-colors hover:text-teal-300 '>About me</a></li>
             <li><a href='#services' className='transition-colors hover:text-teal-300 '>Services</a></li>
